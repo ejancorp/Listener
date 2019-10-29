9bc172d965e2e3aeba7d6c5aee69684f129a158c
Ej Corpuz <ejan.corp@gmail.com>
2019-10-29 16:41:31 +0400

    update spaces

--- listener.js
+++ listener.js
@@ -1,57 +1,57 @@
 const {
-	ipcRenderer
+   ipcRenderer
 } = require('electron');
 const Promise = require('bluebird');
 
 class Listener {
-	constructor() {
-		this.stream = null;
-		this.microphone = null;
-		this.audioContext = new AudioContext();
-		this.analyser = this.audioContext.createAnalyser();
+   constructor() {
+      this.stream = null;
+      this.microphone = null;
+      this.audioContext = new AudioContext();
+      this.analyser = this.audioContext.createAnalyser();
 
-		this.start().then(() => ipcRenderer.send('started'));
-	}
+      this.start().then(() => ipcRenderer.send('started'));
+   }
 
-	start() {
-		return this.stop().then(() => {
-			return navigator.mediaDevices.getUserMedia({
-					audio: true
-				})
-				.then((stream) => this.stream = stream)
-				.then(() => {
-					this.microphone = this.audioContext.createMediaStreamSource(this.stream);
-					this.microphone.connect(this.analyser);
-					this.analyser.connect(this.audioContext.destination);
-				});
-		})
-	}
+   start() {
+      return this.stop().then(() => {
+         return navigator.mediaDevices.getUserMedia({
+               audio: true
+            })
+            .then((stream) => this.stream = stream)
+            .then(() => {
+               this.microphone = this.audioContext.createMediaStreamSource(this.stream);
+               this.microphone.connect(this.analyser);
+               this.analyser.connect(this.audioContext.destination);
+            });
+      })
+   }
 
-	stop() {
-		return Promise.all([
-			this.closeStream(),
-			this.closeAnalyser(),
-			this.closeMicrophone(),
-		]);
-	}
+   stop() {
+      return Promise.all([
+         this.closeStream(),
+         this.closeAnalyser(),
+         this.closeMicrophone(),
+      ]);
+   }
 
-	closeStream() {
-		if (!this.stream) {
-			return Promise.resolve({});
-		}
-		return Promise.map(this.stream.getTracks(), (track) => track.stop());
-	}
+   closeStream() {
+      if (!this.stream) {
+         return Promise.resolve({});
+      }
+      return Promise.map(this.stream.getTracks(), (track) => track.stop());
+   }
 
-	closeAnalyser() {
-		return Promise.resolve(this.analyser.disconnect());
-	}
+   closeAnalyser() {
+      return Promise.resolve(this.analyser.disconnect());
+   }
 
-	closeMicrophone() {
-		if (!this.microphone) {
-			return Promise.resolve({});
-		}
-		return Promise.resolve(this.microphone.disconnect());
-	}
+   closeMicrophone() {
+      if (!this.microphone) {
+         return Promise.resolve({});
+      }
+      return Promise.resolve(this.microphone.disconnect());
+   }
 };
 
 const app = new Listener();
